d77a0da6540cacf967d5151acd35c520e668a053
JUAN FRANCISCO FELIX GONZALEZ <99854187+juanfranxg@users.noreply.github.com>
2026-08-27 11:34:19 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+let player = {
+    name: "Juan",
+    chips: 200,
+}
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": " + "$" + player.chips
+
+function getRandomCard(){
+    let randomCard = Math.floor(Math.random()*13)+1
+
+    if(randomCard === 1){
+        return 11
+    } else if(randomCard > 10){
+        return 10
+    }else{
+        return randomCard
+    }
+}
+
+
+function startGame() {
+    isAlive = true
+    hasBlackJack = false
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " 
+    for(let i = 0; i< cards.length; i++){
+        cardsEl.textContent += cards[i] + ", "
+
+    }
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+
+    }else if (sum === 21 && hasBlackJack === false) {
+        message = "You've got Blackjack!"
+        player.chips += 10
+        hasBlackJack = true
+
+    } else if (sum > 21 && isAlive === true) {
+        message = "You're out of the game!"
+        player.chips -= 20
+        isAlive = false
+    }
+    messageEl.textContent = message
+    playerEl.textContent = player.name + ": " + "$" + player.chips
+
+
+
+}
+
+
+function newCard() {
+    if(isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards)
+        renderGame()
+    }
+
+}
